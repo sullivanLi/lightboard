504261fdffb861976cff1b969e00abe690c3c083
sullivan <future2972001@msn.com>
2015-01-03 17:42:52 +0800

    90 sec

--- app/assets/javascripts/demo.js
+++ app/assets/javascripts/demo.js
@@ -49,7 +49,7 @@ function stopWorker(){
 }
 
 function getColorset(ct){
-	if(ct>81)return;
+	if(ct>94)return;
 	
 	var ct_dot = ct.substr(ct.length - 1)
 	if(ct_dot != 1 && ct_dot != 3 && ct_dot != 5 && ct_dot != 7 && ct_dot != 9){
